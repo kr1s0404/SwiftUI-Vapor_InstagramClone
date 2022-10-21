a8c5c5cb7f108ce6141c04ebefdf37fea975d36e
kr1s0404 <tylerlolz0404@gmail.com>
2022-10-22 06:32:46 +0800

    Add Tab Bar

--- InstagramClient/Instagram/View/ContentView.swift
+++ InstagramClient/Instagram/View/ContentView.swift
@@ -9,13 +9,105 @@ import SwiftUI
 
 struct ContentView: View
 {
+    @State private var selectedTab: Tab = .Home
+    
     var body: some View
     {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selectedTab)
+        {
+            NavigationView
+            {
+                VStack
+                {
+                    
+                }
+            }
+            .tag(Tab.Home)
+            .tabItem { Label(Tab.Home.label, systemImage: Tab.Home.tabIcon) }
+            
+            NavigationView
+            {
+                VStack
+                {
+                    
+                }
+            }
+            .tag(Tab.Search)
+            .tabItem { Label(Tab.Search.label, systemImage: Tab.Search.tabIcon) }
+            
+            NavigationView
+            {
+                VStack
+                {
+                    
+                }
+            }
+            .tag(Tab.Camera)
+            .tabItem { Label(Tab.Camera.label, systemImage: Tab.Camera.tabIcon) }
+            
+            NavigationView
+            {
+                VStack
+                {
+                    
+                }
+            }
+            .tag(Tab.Notification)
+            .tabItem { Label(Tab.Notification.label, systemImage: Tab.Notification.tabIcon) }
+            
+            NavigationView
+            {
+                VStack
+                {
+                    
+                }
+            }
+            .tag(Tab.Profile)
+            .tabItem { Label(Tab.Profile.label, systemImage: Tab.Profile.tabIcon) }
+        }
     }
 }
 
+// MARK: - Tab
+enum Tab: Hashable {
+    case Home
+    case Search
+    case Camera
+    case Notification
+    case Profile
+    
+    var tabIcon: String {
+        switch self {
+        case .Home:
+            return "house"
+        case .Search:
+            return "magnifyingglass"
+        case .Camera:
+            return "camera"
+        case .Notification:
+            return "bell"
+        case .Profile:
+            return "person"
+        }
+    }
+    
+    var label: LocalizedStringKey {
+        switch self {
+        case .Home:
+            return LocalizedStringKey("Home")
+        case .Search:
+            return LocalizedStringKey("Search")
+        case .Camera:
+            return LocalizedStringKey("Post")
+        case .Notification:
+            return LocalizedStringKey("Notification")
+        case .Profile:
+            return LocalizedStringKey("Profile")
+        }
+    }
+}
+
+// MARK: - Preview
 struct ContentView_Previews: PreviewProvider
 {
     static var previews: some View
